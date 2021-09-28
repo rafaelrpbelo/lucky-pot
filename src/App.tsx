3d0ec786d7cbffa1b00b8688cf1dba238c073c0c
Rafael Belo <rafaelrpbelo@gmail.com>
2021-09-27 23:23:13 -0300

    Create Dashboard and Summary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "./styles/global"
 import { Header } from "./components/Header"
+import { Dashboard } from "./components/Dashboard"
 
 export function App() {
   return (
     <div className="App">
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </div>
   );

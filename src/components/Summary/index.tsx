79e4d756a801a59e137ab113180a9ff8ab44bd58
Rafael Belo <rafaelrpbelo@gmail.com>
2021-10-06 10:36:35 -0300

    Display totals on Summary component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,43 @@
-import incomeImg from "../../assets/income.svg"
-import outcomeImg from "../../assets/outcome.svg"
-import totalImg from "../../assets/total.svg"
+import { useState, useEffect, useContext } from "react";
 
-import { Container } from "./styles"
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+
+import { Container } from "./styles";
+
+interface SummaryTotal {
+  withdraw: number;
+  deposit: number;
+  total: number;
+}
 
 export function Summary() {
+  const transactions = useContext(TransactionsContext);
+
+  const [total, setTotal] = useState<SummaryTotal>({withdraw: 0, deposit: 0, total: 0});
+
+  const updateTotals = () => {
+    setTotal(
+      transactions.reduce((acc: SummaryTotal, item) => {
+        if (item.type === "withdraw") {
+          acc.withdraw += item.amount;
+        }
+        else if (item.type === "deposit") {
+          acc.deposit += item.amount;
+        }
+
+        acc.total += item.amount;
+
+        return acc;
+      }, {withdraw: 0, deposit: 0, total: 0})
+    )
+  }
+
+  useEffect(updateTotals, [transactions])
+
   return (
     <Container>
       <div>
@@ -12,7 +45,10 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+
+        <strong>
+          {Intl.NumberFormat("pt-br", {style: "currency", currency: "BRL"}).format(total.deposit / 100)}
+        </strong>
       </div>
 
       <div>
@@ -20,7 +56,10 @@ export function Summary() {
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+
+        <strong>
+          {Intl.NumberFormat("pt-br", {style: "currency", currency: "BRL"}).format(total.withdraw / 100)}
+        </strong>
       </div>
 
       <div className="highlight-background">
@@ -28,7 +67,10 @@ export function Summary() {
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$1000,00</strong>
+
+        <strong>
+          {Intl.NumberFormat("pt-br", {style: "currency", currency: "BRL"}).format(total.total / 100)}
+        </strong>
       </div>
     </Container>
   )
